6188ffe95149df249193009a5c342c9181bb3891
picugeo27 <61882288+picugeo27@users.noreply.github.com>
2024-10-10 14:27:13 +0200

    Movimiento provisional
    Esta el script de movimiento, pero tiene pinta de que necesita arreglos. Me di cuenta que estaba en la main asi que lo subo y me muevo a mi rama

---
+++ LaRana/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float dragForce = 1f;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 direction;
+
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.freezeRotation = true;
+        _rb.drag = dragForce;
+    }
+
+    void Update()
+    {
+        handleInput();
+    }
+
+    private void FixedUpdate()
+    {
+        movePlayer();
+    }
+
+    void handleInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    void movePlayer()
+    {
+        direction = new Vector3(horizontalInput, 0, verticalInput);
+        _rb.AddForce(direction * speed * Time.deltaTime, ForceMode.VelocityChange);
+    }
+}
